8a93b0001918beeb117cb951112af61f0b636923
Kevincho9898 <47290520+Kevincho9898@users.noreply.github.com>
2019-02-24 01:51:01 -0800

    Create angle-lines.js

---
+++ angle-lines.js
@@ -0,0 +1,13 @@
+function setup() {
+  createCanvas(400, 400);
+  colorMode(HSB);
+}
+
+function draw() {
+  background(255);
+
+x
+  for (var x = width-310; x < width+10; x = x + 11) {
+    line(x, height/2, x+100, height/2-75, );
+  }
+}
